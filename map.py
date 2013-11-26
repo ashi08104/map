489107349ade6ffaa9f535182e6da1a538e21cae
ashi <ashi08104@gmail.com>
2013-11-26 08:52:42 +0800

    clean code.

--- map.py
+++ map.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/local/bin/python
 # -*- coding: utf-8 -*-
 
 import codecs
@@ -26,26 +26,6 @@ def yes_or_no(message):
     else:
         return False
 
-# def draw(file_name, data):
-#     '''
-#     draw the data to map
-#     '''
-#     i = 0
-#     for bus in data:
-#         #bus info: [#no., name, time]
-#         bus_info = bus[:3]
-#         start = bus[3]
-#         stations = bus[4].split(',')
-#         if u'(直驶)' in stations:
-#             stations = []
-#         end = bus[5]
-#         #draw_kml_bus_line(bus_info, start, end, stations)
-#         draw_station_start(bus_info, start)
-#         print i
-#         i = i + 1
-
-#     kml.save(file_name)    
-
 def draw(kml_filename, data):
     '''
     draw the date to kml file
@@ -64,45 +44,6 @@ def draw(kml_filename, data):
 def draw_station(info, coord):
     kml.newpoint(name = info,
                  coords = [coord])
-
-
-#this function is deprecated. we should get geo date from previous step
-#not from google map online.
-def draw_kml_bus_line(info, start, end, stations):
-    start_geo = [start]
-    start_geo.append(get_coordinate(start))
-    end_geo = [end]
-    end_geo.append(get_coordinate(end))
-    station_geos = []
-    if stations:
-        for station in stations:
-            station_geo = [station]
-            station_geo.append(get_coordinate(station))
-            station_geos.append(station_geo)
-    #draw bus line
-    coords = [(start_geo[1][0], start_geo[1][1])]
-    if station_geos:
-        for station_geo in station_geos:
-            coords.append((station_geo[1][0],
-                           station_geo[1][1]))
-    coords.append((end_geo[1][0], end_geo[1][1]))
-    kml.newlinestring(name = info[1],
-                      description = 'No.' + info[0] + ' time:' + info[2],
-                      coords = coords)
-    
-    #draw bus stations
-    kml.newpoint(name = start_geo[0],
-                 coords = [(start_geo[1][0],
-                            start_geo[1][1])])
-    if station_geos:
-        for station_geo in station_geos:
-            kml.newpoint(name = station_geo[0],
-                         coords = [(station_geo[1][0],
-                                    station_geo[1][1])])
-    kml.newpoint(name = end_geo[0],
-                 coords = [(end_geo[1][0],
-                            end_geo[1][1])])
-
 def main():
     #TODO read from commandline
     msg1 = "Do we have geography data in save_geo.data?"
